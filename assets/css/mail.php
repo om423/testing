1dae609591218ff30a264548ffa5449e3e2dbae2
om423 <103942531+om423@users.noreply.github.com>
2022-05-07 16:51:49 -0400

    Add files via upload

---
+++ assets/css/mail.php
@@ -0,0 +1,18 @@
+<?php
+$name=$_POST['name']
+$email=$_POST['email']
+$message=$_POST['issue']
+
+$to="journalhsdiscovery@gmail.com"
+
+$subject="Mail from Website"
+$txt="Name=" . $name . "\r\n Email= ".$email . "\r\n Message=" . $message;
+$headers = "From: noreply@yoursite.com" . "\r\n" .
+"CC: somebodyelse@example.com";
+if($email!=NULL)
+    mail($to,$subject,$txt,$headers);
+
+}
+//redirect
+header("Location:thankyou.html");
+?>
---
+++ pages/documentation.md
@@ -0,0 +1,6 @@
+---
+layout: profilecategory
+title: Editors
+permalink: /editors
+---
+
